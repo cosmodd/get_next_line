c228eed6f7bbb6db9231190bb5cae8a001e0e8ce
mrattez <mrattez@student.42nice.fr>
2021-11-04 10:02:07 +0100

    Bonus part

---
+++ get_next_line_bonus.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 11:56:03 by mrattez           #+#    #+#             */
+/*   Updated: 2021/11/03 13:55:20 by mrattez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_until_nl_eof(int fd, char *buffer, char *next_content[])
+{
+	char	*temp_content;
+	int		read_size;
+
+	read_size = 1;
+	while (read_size)
+	{
+		read_size = read(fd, buffer, BUFFER_SIZE);
+		if (read_size < 0)
+			return (NULL);
+		if (read_size == 0)
+			break ;
+		buffer[read_size] = 0;
+		if (!next_content[fd])
+			next_content[fd] = ft_strdup("");
+		temp_content = next_content[fd];
+		next_content[fd] = ft_strjoin(temp_content, buffer);
+		free(temp_content);
+		temp_content = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (next_content[fd]);
+}
+
+char	*update_line(char *line)
+{
+	int		nl_index;
+	char	*remains;
+
+	nl_index = 0;
+	while (line[nl_index] && line[nl_index] != '\n')
+		nl_index++;
+	if (!line[nl_index])
+		return (NULL);
+	remains = ft_substr(line, nl_index + 1, ft_strlen(line) - nl_index);
+	if (!remains[0])
+	{
+		free(remains);
+		remains = NULL;
+	}
+	line[nl_index + 1] = 0;
+	return (remains);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*next_content[FOPEN_MAX];
+	char		*buffer;
+	char		*line;
+
+	if (read(fd, NULL, 0))
+		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	line = read_until_nl_eof(fd, buffer, next_content);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	next_content[fd] = update_line(line);
+	return (line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 15:23:47 by mrattez           #+#    #+#             */
+/*   Updated: 2021/11/03 11:55:08 by mrattez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	c;
+
+	c = 0;
+	while (str[c])
+		c++;
+	return (c);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*dst;
+	size_t	sublen;
+	size_t	i;
+
+	if (start > (unsigned int) ft_strlen(s))
+		return (ft_strdup(""));
+	i = 0;
+	sublen = 0;
+	while (s[start + sublen] && sublen < len)
+		sublen++;
+	dst = malloc((sublen + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	while (i < sublen)
+	{
+		dst[i] = s[start + i];
+		i++;
+	}
+	dst[i] = 0;
+	return (dst);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*out;
+	char	search;
+
+	out = (char *)s;
+	search = c;
+	while (*out)
+	{
+		if (*out == search)
+			return (out);
+		out++;
+	}
+	if (*out == c)
+		return (out);
+	return ((void *)0);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+	dest = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = 0;
+	return (dest);
+}
